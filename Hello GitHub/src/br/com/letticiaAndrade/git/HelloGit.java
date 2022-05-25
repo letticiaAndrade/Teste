d46479c8098151881edf1ce137c73dd59d9ae54a
Letícia Andrade <96126803+letticiaAndrade@users.noreply.github.com>
2022-05-25 13:29:18 -0300

    Adicionei arquivos

---
+++ Hello GitHub/src/br/com/letticiaAndrade/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.letticiaAndrade.git;
+
+/**
+ * @author jefer
+ * Exemplo de integração com o GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
